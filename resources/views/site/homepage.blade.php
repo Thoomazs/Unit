bd5d5a0dee003f64c26b9bfc381a268cff261b45
Tomas Novotny <thomazs.novotny@gmail.com>
2015-04-21 17:17:00 +0200

    misc

--- resources/views/site/homepage.blade.php
+++ resources/views/site/homepage.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 
-    HP
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2" style="font-size: 20px;">
+            <p class="text-justify">Webová aplikace CN Agile umožňuje realizovat nejčastější
+                                    techniky agilní metodiky "SCRUM", jako jsou "Poker Planning" a "Retrospektiva". Distribuované týmy tak mohou pohodlně spolupracovat
+                                    v reálném čase bez pořeby fyzické komunikace.</p>
+
+            <hr/>
+            <p class="text-justify">Poker Planning tým využije na úplném začátku vývoje, umožní stanovit časový rámec projektu a minimalizuje šanci nepříjemných
+               překvapení například z důvodu překročení domluveného rozpočtu.</p>
+            <hr/>
+            <p class="text-justify">Retrospektivu na druhou stranu tým využije v úplném závěru vývoje, kdy se hodnotí spolupráce interních vývojářů.
+               V rámci retrospektivního hodnocení minulých sprintů je možné zlepšit procesy a celkově zefektivnit práci.</p>
+        </div>
+    </div>
+
 
 @stop
